13ed1dd2db62c986502da7409b13144843b0432d
Lukas Pinheiro <lukas.pinheiro123@gmail.com>
2025-06-20 21:39:09 -0400

    Implementacao da tela inicial.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js']) 
+    @vite(['resources/css/app.css', 'resources/js/app.tsx'])
+    @yield('head')
 </head>
-<body class="bg-gray-100">
-    @include('partials.navbar')
-    <main class="py-8">
-        @yield('content') 
-    </main>
+<body class="relative bg-[url('/images/fundo-painel-solar.jpg')] bg-cover bg-center min-h-screen backdrop-blur-sm">
+
+    <div class="absolute inset-0 bg-black/50"></div>
+
+    <div class="relative z-10">
+        @include('partials.navbar')
+
+        <main>
+            @yield('content')
+        </main>
+    </div>
 
 </body>
+
 </html>
